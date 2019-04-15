65c95a0673c93bf4898386c02fd5e7db518f087a
gadocto <gadocto@gmail.com>
2019-04-15 11:40:06 +0300

    Added some cmds. Abou time to start labels

--- assembly/src/cmdspec.rs
+++ assembly/src/cmdspec.rs
@@ -1,7 +1,9 @@
 use std::collections::HashMap;
 use std::vec::Vec;
+use std::io;
 
 pub type Word = u32;
+pub type DWord = u64;
 
 #[derive(Clone)]
 pub enum CmdFormat { RM, RR, RI }
@@ -32,23 +34,23 @@ impl CmdTable {
 
         macro_rules! RR {
             ($word:expr) => { (
-                (($word >> 8)  & 0b1111u32), 
-                (($word >> 12) & 0b1111u32), 
-                ($word >> 16)
+                (($word >> 8)  & 0b1111u32) as usize, 
+                (($word >> 12) & 0b1111u32) as usize, 
+                (($word.clone() as i32) >> 16) as u32
                 ) }
         }
 
         macro_rules! RM {
             ($word:expr) => { (
-                (($word >> 8) & 0b1111u32), 
+                (($word >> 8) & 0b1111u32) as usize, 
                 ($word >> 12)
                 ) }
         }
 
         macro_rules! RI {
             ($word:expr) => { (
-                (($word >> 8) & 0b1111u32), 
-                ($word >> 12u32)
+                (($word >> 8) & 0b1111u32) as usize, 
+                (($word.clone() as i32) >> 12) as u32
                 ) }
         }
 
@@ -58,20 +60,103 @@ impl CmdTable {
 
             match imm {
                 0 => cpu.halt = true,
-                102 => println!("{}", cpu.r[reg as usize]),
+                100 => {
+                    let mut in_txt = String::new();
+                    io::stdin().read_line(&mut in_txt).expect("Failed to read from stdin");
+                    cpu.r[reg] = in_txt.trim().parse::<u32>().expect("Failed to parse stdin");
+                },
+                102 => println!("{}", cpu.r[reg]),
                 _ => panic!("Bad syscall argument!")
             }
         });
-/*        table.insert("add", 2, RR, &|cpu, arg| {
+        table.insert("add", 2, RR, &|cpu, arg| {
             let (r1, r2, imm) = RR!(arg);
 
-            cpu.r[r1] = cpu.r[r2] + imm;
+            cpu.r[r1] += cpu.r[r2].wrapping_add(imm);
         });
         table.insert("addi", 3, RI, &|cpu, arg| {
             let (reg, imm) = RI!(arg);
 
-            cpu.r[reg] = cpu.r[reg] + imm;
-        });*/
+            cpu.r[reg] = cpu.r[reg].wrapping_add(imm);
+        });
+        table.insert("sub", 4, RR, &|cpu, arg| {
+            let (r1, r2, imm) = RR!(arg);
+
+            cpu.r[r1] -= cpu.r[r2].wrapping_add(imm);
+        });
+        table.insert("subi", 5, RI, &|cpu, arg| {
+            let (reg, imm) = RI!(arg);
+
+            cpu.r[reg] = cpu.r[reg].wrapping_sub(imm);
+        });
+        table.insert("mul", 6, RR, &|cpu, arg| {
+            let (r1, r2, _) = RR!(arg);
+
+            let mul: DWord = (cpu.r[r1] as u64) * (cpu.r[r2] as u64);
+            cpu.r[r1] = (mul & 0b11111111111111111111111111111111) as u32;
+            cpu.r[r1 + 1] = (mul >> 32) as u32;
+        });
+        table.insert("muli", 7, RI, &|cpu, arg| {
+            let (reg, imm) = RI!(arg);
+
+            let mul: DWord = (cpu.r[reg] as u64) * (imm as u64);
+            cpu.r[reg] = (mul & 0b11111111111111111111111111111111) as u32;
+            cpu.r[reg + 1] = (mul >> 32) as u32;
+        });
+        table.insert("div", 8, RR, &|cpu, arg| {
+            let (r1, r2, _) = RR!(arg);
+
+            let q: u32 = cpu.r[r1] / cpu.r[r2];
+            let r: u32 = cpu.r[r1] % cpu.r[r2];
+
+            cpu.r[r1] = q;
+            cpu.r[r1 + 1] = r;
+        });
+        table.insert("divi", 9, RI, &|cpu, arg| {
+            let (reg, imm) = RI!(arg);
+
+            let r = cpu.r[reg] % imm;
+            cpu.r[reg] /= imm;
+            cpu.r[reg + 1] = r;
+        });
+        table.insert("lc", 12, RI, &|cpu, arg| {
+            let (reg, imm) = RI!(arg);
+
+            cpu.r[reg] = imm;
+        });
+        table.insert("shl", 13, RR, &|cpu, arg| {
+            let (r1, r2, _) = RR!(arg);
+
+            cpu.r[r1] <<= cpu.r[r2];
+        });
+        table.insert("shli", 14, RI, &|cpu, arg| {
+            let (reg, imm) = RI!(arg);
+
+            cpu.r[reg] <<= imm;
+        });
+        table.insert("shr", 15, RR, &|cpu, arg| {
+            let (r1, r2, _) = RR!(arg);
+
+            cpu.r[r1] >>= cpu.r[r2]
+        });
+        table.insert("shri", 16, RI, &|cpu, arg| {
+            let (reg, imm) = RI!(arg);
+
+            cpu.r[reg] >>= imm;
+        });
+        table.insert("and", 17, RR, &|cpu, arg| {
+            let (r1, r2, _) = RR!(arg);
+
+            cpu.r[r1] &= cpu.r[r1];
+        })
+        table.insert("andi", 18, RI, &|cpu, arg| {
+
+        });
+        table.insert("jmp", 46, RM, &|cpu, arg| {
+            let (_, mem) = RM!(arg);
+
+            cpu.r[15] = mem;
+        });
 
         table
     }
--- assembly/src/main.rs
+++ assembly/src/main.rs
@@ -3,10 +3,6 @@ mod cmdspec;
 mod procexec;
 
 fn main() {
-    let mut res = txtparse::parsecode("syscall r10 0\n halt r10 -1");
+    let mut res = txtparse::parsecode("syscall r8 100\n divi r8 2\n syscall r8 102\n syscall r9 102\n halt r10 0");
     res.exec();
-
-    for cmd in res.state.mem {
-    	println!("{:#018b}\n", cmd);
-    }
 }
